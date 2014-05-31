b1a665cb1514477c8910e2ed3965112e97a2d253
Adam Tal <admtal@gmail.com>
2014-05-31 20:29:47 +0000

    outfits

---
+++ angel/outfits/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+###
+from angel.models import BaseModel
+
+
+class Outfit(BaseModel):
+    """A picture of a users outfit, to be rated by a group of trusted friends"""
+    user = models.ForeignKey(User, related_name='outfits')
+    picture = models.ImageField(upload_to='outfit_images', blank=True, help_text='picture of outfit')
+    message = models.CharField(max_length='256', blank=True, help_text='message to include with picture')
+
+
+class OutfitRating(BaseModel):
+    """A users rating for an outfit"""
+    user = models.ForeignKey(User, related_name='ratings')
+    score = models.IntegerField()
+    comment = models.CharField(max_length='256', blank=True, help_text='Comment to include with rating')
+    outfit = models.ForeignKey('Outfit', help_text='Outfit being rated')
+
+    # isexpired ... check for date_created + 2 min
+
--- angel/users/models.py
+++ angel/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     """Extra information to track for each user"""
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, related_name='profile')
 
     picture = models.ImageField(upload_to='profile_images', blank=True, help_text='Your pretty face')
 
