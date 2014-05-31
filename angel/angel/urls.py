2f787a1aa5c4f71e1f04c9fa073746841c23a079
Adam Tal <admtal@gmail.com>
2014-05-31 19:52:23 +0000

    more user stuff

--- angel/angel/urls.py
+++ angel/angel/urls.py
@@ -7,7 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^$', 'angel.views.home', {'template': 'home.html'}, name='home'),
                        url(r'^about/$', 'angel.views.about', {'template': 'about.html'}, name='about'),
-
-
+                       url(r'^register/$', 'users.views.register', {'template': 'register.html'}, name='register'),
+                       url(r'^login/$', 'users.views.user_login', {'template': 'login.html'}, name='login'),
+                       url(r'^logout/$', 'users.views.user_logout', name='logout'),
                        url(r'^admin/', include(admin.site.urls)),
 )
--- angel/users/models.py
+++ angel/users/models.py
@@ -7,4 +7,6 @@ class UserProfile(models.Model):
     """Extra information to track for each user"""
     user = models.OneToOneField(User)
 
+    picture = models.ImageField(upload_to='profile_images', blank=True, help_text='Your pretty face')
+
     pushover_key = models.CharField(max_length=256, help_text='account key from pushover')
