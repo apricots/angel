ab45a1a702b50357c29d8887f2d42b577571cd4c
Adam Tal <admtal@gmail.com>
2014-05-31 22:13:48 -0400

    dfd

--- angel/settings/base.py
+++ angel/settings/base.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dj_static',
     # 'south',
     'users',
     'outfits',
