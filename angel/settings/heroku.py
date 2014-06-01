ae3edc0f2185f74baaf7c50ba947be471a93397b
Adam Tal <admtal@gmail.com>
2014-05-31 22:25:21 -0400

    f

--- angel/settings/heroku.py
+++ angel/settings/heroku.py
@@ -1,6 +1,11 @@
+from base import *
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -15,5 +20,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,  'static'),
 )
